d0236a1558af41653f03a3ded3cf56196f61f09b
Iris Leal <iris_leal_m@hotmail.com>
2020-06-17 13:32:06 -0700

    Basic high level set up, including one model

--- trip/models.py
+++ trip/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.urls import reverse
 
-# Create your models here.
+class Destinations(models.Model):
+    author = models.ForeignKey(get_user_model(), on_delete = models.CASCADE)
+    place = models.CharField(max_length = 50)
+    location = models.CharField(max_length = 35)
+    summary = models.TextField()
+
+    def __str__(self):
+      return self.place
