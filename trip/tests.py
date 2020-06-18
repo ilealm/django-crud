c6abc84ccac4ff6ef1d62312aff1bad8d882c633
Iris Leal <iris_leal_m@hotmail.com>
2020-06-17 17:39:39 -0700

    test setup

--- trip/tests.py
+++ trip/tests.py
@@ -1,3 +1,20 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Destinations
+
+class TripTest(TestCase):
+  def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username='tester',
+            email='tester@email.com',
+            password='pass'
+        )
+
+        self.destination = Destinations.objects.create(
+            author=self.user,
+            place='island',
+            location='in the sea',
+            summary='is a magical place',
+        )
